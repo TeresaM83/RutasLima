5033ccca0795c95e558461af85fd5e95ec21d592
TeresaMartinez100379 <101592307+TeresaM83@users.noreply.github.com>
2024-04-01 16:08:59 -0500

    Primer commit

---
+++ ClientApp/src/index.js
@@ -0,0 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import App from './App';
+
+const rootElement = document.getElementById('root');
+const root = React.createRoot(rootElement)
+
+root.render(
+    <App />
+ );
